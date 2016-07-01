19ccc191bcb75805c87e4f9509ed6ba4b6b310bf
Gear61 <chessnone@yahoo.com>
2016-07-01 10:46:02 -0700

    Remove loading dialog

--- app/src/main/java/randomappsinc/com/sqlpractice/Activities/WebActivity.java
+++ app/src/main/java/randomappsinc/com/sqlpractice/Activities/WebActivity.java
@@ -3,13 +3,10 @@ package randomappsinc.com.sqlpractice.Activities;
 import android.annotation.TargetApi;
 import android.os.Bundle;
 import android.view.View;
-import android.webkit.WebChromeClient;
 import android.webkit.WebResourceError;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-
-import com.afollestad.materialdialogs.MaterialDialog;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -26,8 +23,6 @@ public class WebActivity extends StandardActivity {
     @Bind(R.id.parent) View parent;
     @Bind(R.id.webview) WebView webView;
 
-    private MaterialDialog loadingPage;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,23 +33,9 @@ public class WebActivity extends StandardActivity {
         String idea = getIntent().getStringExtra(IDEA_KEY);
         setTitle(idea);
 
-        loadingPage = new MaterialDialog.Builder(this)
-                .title(R.string.page_load_progress)
-                .content(R.string.loading_material)
-                .progress(false, 100, true)
-                .show();
-
         webView.getSettings().setJavaScriptEnabled(true);
         webView.getSettings().setBuiltInZoomControls(true);
 
-        webView.setWebChromeClient(new WebChromeClient() {
-            public void onProgressChanged(WebView view, int progress) {
-                loadingPage.setProgress(progress);
-                if (progress == 100 && loadingPage.isShowing()) {
-                    loadingPage.dismiss();
-                }
-            }
-        });
         webView.setWebViewClient(new WebViewClient() {
             @SuppressWarnings("deprecation")
             @Override
